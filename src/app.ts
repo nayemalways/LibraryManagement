228424efdf7727f2214de16852663951a0564aaf
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-09 18:41:07 +0600

    added cors configaration

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://libraryapp-three-iota.vercel.app"
+    origin: "http://localhost:5173"
 }))
 app.use(helmet());
 app.use(hpp());
