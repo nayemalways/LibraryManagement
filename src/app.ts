56d474851f3aa9b2fac6f624b3eb8430f7a4d220
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-10 18:17:50 +0600

    configerd agian

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,17 @@ import borrowRouter from './app/router/borrow.router';
 const app = express();
 
 // Middleware
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || ["http://localhost:5173"].includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
  
 app.use(helmet());
 app.use(hpp());
